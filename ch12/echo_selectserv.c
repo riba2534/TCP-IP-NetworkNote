8b20f9f6f8b99903e06a87063709cc1af1abf805
riba2534 <riba2534@qq.com>
2019-01-23 17:31:10 +0800

    完成了第 12 章 I/O 复用

---
+++ ch12/echo_selectserv.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+#define BUF_SIZE 100
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    struct timeval timeout;
+    fd_set reads, cpy_reads;
+
+    socklen_t adr_sz;
+    int fd_max, str_len, fd_num, i;
+    char buf[BUF_SIZE];
+    if (argc != 2)
+    {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("bind() error");
+    if (listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    FD_ZERO(&reads);
+    FD_SET(serv_sock, &reads); //注册服务端套接字
+    fd_max = serv_sock;
+
+    while (1)
+    {
+        cpy_reads = reads;
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 5000;
+
+        if ((fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout)) == -1) //开始监视,每次重新监听
+            break;
+        if (fd_num == 0)
+            continue;
+
+        for (i = 0; i < fd_max + 1; i++)
+        {
+            if (FD_ISSET(i, &cpy_reads)) //查找发生变化的套接字文件描述符
+            {
+                if (i == serv_sock) //如果是服务端套接字时,受理连接请求
+                {
+                    adr_sz = sizeof(clnt_adr);
+                    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &adr_sz);
+
+                    FD_SET(clnt_sock, &reads); //注册一个clnt_sock
+                    if (fd_max < clnt_sock)
+                        fd_max = clnt_sock;
+                    printf("Connected client: %d \n", clnt_sock);
+                }
+                else //不是服务端套接字时
+                {
+                    str_len = read(i, buf, BUF_SIZE); //i指的是当前发起请求的客户端
+                    if (str_len == 0)
+                    {
+                        FD_CLR(i, &reads);
+                        close(i);
+                        printf("closed client: %d \n", i);
+                    }
+                    else
+                    {
+                        write(i, buf, str_len);
+                    }
+                }
+            }
+        }
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
